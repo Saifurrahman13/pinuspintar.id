9fd3210a5387eb75997abfe1504db312e21f0ad2
ipul <saifurrahmanboenk98@gmail.com>
2023-12-08 19:16:42 +0700

    ini tugas html dasar

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Toggel Class Active
+const navbarNav = document.querySelector('.navbar-nav');
+// Ketika Hamburger Menu di Klik
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+// Klik diluar Sidebar Untuk Menghilangkan Nav
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function (e) {
+    if (!hamburger.contains(e.target) && !navbarNav.contains (e.target)) {
+        navbarNav.classList.remove('active');
+    }
+});
